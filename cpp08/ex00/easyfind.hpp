5b059572e5848eb3757471ecf200e26842befb72
Valeriia Fedorova <vfedorov@z1t5c1.42bangkok.com>
2024-08-22 16:56:57 +0700

    smt

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,18 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <exception>
+
+//copying
+
+template <typename T>
+class easyFind
+{
+	private:
+
+	public:
+}
+
+# include "easyfind.tpp"
+#endif
